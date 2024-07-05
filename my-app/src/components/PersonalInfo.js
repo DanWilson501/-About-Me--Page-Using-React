75ef406ebacc41cd00e6e4a5ae1c6d98206726c4
Daniel Wilson <danwilson501@gmail.com>
2024-07-05 17:02:22 -0400

    Personal Info component created as intro page to display name, biography and contact info

---
+++ my-app/src/components/PersonalInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PersonalInfo = () => (
+    <header className="App-header">
+        <h1>Daniel Wilson</h1>
+        <p>Studying at University of South Florida, I am ambitious in developing my abilities in programming for applications utilized in home, business, medical and every day needs.</p>
+        <p>Email: danielexample@email.com | LinkedIn: linkedin.com/in/daniel@usf</p>
+    </header>
+);
+
+export default PersonalInfo;
