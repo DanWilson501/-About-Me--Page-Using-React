e621f1a0d0a4c2db98c9c0bfe2d72914e9d90a73
Daniel Wilson <danwilson501@gmail.com>
2024-07-05 23:07:05 -0400

    css styling improved to reflect current institution colors and to display a sleeker design. Comments added to each segment of the code to provide context.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 // U5223-1368
 
+//Import Rect, CSS, and JS files. Implements JSX features and integrates multiple components.
 import React from 'react';
 import './App.css';
 import PersonalInfo from './components/PersonalInfo';
@@ -7,7 +8,9 @@ import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Education from './components/Education';
 
+//Function containing multiple arrays of different properties.
 function App() {
+  //Array consisting of id, name and level utilizing the prop from Skills.js
     const skills = [
         { id: 1, name: 'React', level: 'Beginner' },
         { id: 2, name: 'Python', level: 'Beginner' },
@@ -16,17 +19,17 @@ function App() {
         { id: 5, name: 'CSS', level: 'Advanced' },
         { id: 6, name: 'HTML', level: 'Expert' }
     ];
-
+  //Consisting of id, title, company and duration utilizing the prop from experience.js
     const experiences = [
       { id: 1, title: 'Tech Team Member', company: 'Target', duration: 'May 2021 – December 2023' },
       { id: 2, title: 'Student Web Developer', company: 'USF Business Application Development', duration: 'Jun 2024 – Present' }
     ];
-
+  //Consisting of id, institution, degree and years utilizing the prop from education.js
     const education = [
       { id: 1, institution: 'Hillsborough Community College', degree: 'B.S. Business Analytics and Information Systems', years: '2021 – 2023' },
-      { id: 2, institution: 'University of South Florida', years: '2024 – Present' }
+      { id: 2, institution: 'University of South Florida', years: '2024 – 2026' }
     ];
-
+  //Container that manages the properties within the other modules and applies the data to the structure of the page.
   return (
       <div className="App">
           <PersonalInfo />
--- my-app/src/components/PersonalInfo.js
+++ my-app/src/components/PersonalInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PersonalInfo = () => (
     <header className="App-header">
         <h1>Daniel Wilson</h1>
-        <p>Studying at University of South Florida, I am ambitious in developing my abilities in programming for applications utilized in home, business, medical and every day needs.</p>
+        <p>Studying at University of South Florida, I am ambitious in developing a skillset in programming for applications utilized in home, business, medical and every day needs.</p>
         <p>Email: danielexample@email.com | LinkedIn: linkedin.com/in/daniel@usf</p>
     </header>
 );
